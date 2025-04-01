222f9511e3713e12ce492112236876f77522aad3
TBZ <tranbaoonguyen.tb@gmail.com>
2025-04-01 22:37:06 +0700

    add modal

---
+++ BTL LTW PRO/BTL LTW PRO/Models/Assignment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BTL_LTW_PRO.Models
+{
+    public class Assignment
+    {
+        [Key]
+        public int AssignmentID { get; set; }
+
+        [ForeignKey("Lesson")]
+        public int LessonID { get; set; }
+        public Lesson lesson { get; set; } = new Lesson();
+
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = "";
+        public string? Description;
+        public DateTime? DueDate { get; set; }
+    }
+}
