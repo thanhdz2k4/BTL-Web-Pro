80a71eae01e2f5503bef7ae9127ba0ee4067ca3b
TBZ <tranbaoonguyen.tb@gmail.com>
2025-04-07 21:54:46 +0700

    update'

---
+++ BTL LTW PRO/BTL LTW PRO/Models/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BTL_LTW_PRO.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Message { get; set; } = "";
+
+        public bool IsRead { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+    }
+}
