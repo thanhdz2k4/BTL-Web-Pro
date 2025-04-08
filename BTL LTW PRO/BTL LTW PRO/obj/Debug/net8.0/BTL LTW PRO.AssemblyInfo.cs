47825b30142921d969afa0dec42b00242e87722b
thuy20112004 <164833195+thuy20112004@users.noreply.github.com>
2025-04-08 15:29:42 +0700

    a1

--- BTL LTW PRO/BTL LTW PRO/obj/Debug/net8.0/BTL LTW PRO.AssemblyInfo.cs
+++ BTL LTW PRO/BTL LTW PRO/obj/Debug/net8.0/BTL LTW PRO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL LTW PRO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef8d411465e5efdfbeaec8c82ff1bc4470adcd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7565a70be3f3330b010c4cdcea5b4c8f5aee9bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL LTW PRO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL LTW PRO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
